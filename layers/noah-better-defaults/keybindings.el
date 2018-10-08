55c70a3bf7a2022d298fe669af74ed5fc7c51528
Noah <340545644@qq.com>
2018-10-08 20:50:25 +0800

    use gdb as default debugger.

--- layers/noah-better-defaults/keybindings.el
+++ layers/noah-better-defaults/keybindings.el
@@ -241,19 +241,13 @@
 (spacemacs/set-leader-keys "t t" 'tabbar-mode)
 
 ;; set asm-mode
-(cond ((and (spacemacs/system-is-mswindows) window-system)
-       (spacemacs/set-leader-keys-for-major-mode 'asm-mode "'" 'debug))
-      ((spacemacs/system-is-linux)
-       (spacemacs/set-leader-keys-for-major-mode 'asm-mode "'" 'gdb)))
+(spacemacs/set-leader-keys-for-major-mode 'asm-mode "'" 'gdb)
 
 ;; delete four spaces and semi in nasm, c, c++, java, octave, matlab mode.
 (add-hook 'nasm-mode-hook
           '(lambda ()
              (define-key nasm-mode-map (kbd "<backspace>") 'python-indent-dedent-line-backspace)))
-(cond ((and (spacemacs/system-is-mswindows) window-system)
-       (spacemacs/set-leader-keys-for-major-mode 'nasm-mode "'" 'debug))
-      ((spacemacs/system-is-linux)
-       (spacemacs/set-leader-keys-for-major-mode 'nasm-mode "'" 'gdb)))
+(spacemacs/set-leader-keys-for-major-mode 'nasm-mode "'" 'gdb)
 
 (add-hook 'c-mode-hook
           '(lambda ()
