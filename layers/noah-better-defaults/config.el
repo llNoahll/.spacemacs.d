911dd782277535d0f22df2a160eb4322d89a3997
Noah <340545644@qq.com>
2019-07-20 21:25:02 +0800

    edit .sps file as scheme mode.

--- layers/noah-better-defaults/config.el
+++ layers/noah-better-defaults/config.el
@@ -48,6 +48,7 @@
 
 ;; add auto-mode-lists
 (add-to-list 'auto-mode-alist '("\\.sls\\'"       . scheme-mode))
+(add-to-list 'auto-mode-alist '("\\.sps\\'"       . scheme-mode))
 (add-to-list 'auto-mode-alist '("\\.snippet\\'"   . snippet-mode))
 (add-to-list 'auto-mode-alist '("\\.yasnippet\\'" . snippet-mode))
 (add-to-list 'auto-mode-alist '("\\.scrbl\\'"     . racket-mode))
@@ -139,7 +140,7 @@
              (vline-mode t)
              (highlight-indentation-mode t)
              (treemacs--setup-icon treemacs-icon-tex      "~/.spacemacs.d/packages/treemacs/icons/tex.png"    "tex")
-             (treemacs--setup-icon treemacs-icon-scheme   "~/.spacemacs.d/packages/treemacs/icons/scheme.png"    "ss" "scm" "sls")
+             (treemacs--setup-icon treemacs-icon-scheme   "~/.spacemacs.d/packages/treemacs/icons/scheme.png"    "ss" "scm" "sls" "sps")
              (treemacs--setup-icon treemacs-icon-racket   "~/.spacemacs.d/packages/treemacs/icons/racket.png"    "racket" "rkt" "rktl" "rktd" "scrbl" "scribble" "plt")))
 
 ;; set c-mode
